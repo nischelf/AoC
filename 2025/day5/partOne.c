df604177500036e9fb8321b136cd3aafa0c66a82
nischelf <nischelwitzer.fabian@a1.net>
2025-12-08 16:46:55 +0100

    fix
    do not store every single value, but the range and check later the
    boundaries

--- 2025/day5/partOne.c
+++ 2025/day5/partOne.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
 
   FILE *pfile;
   char buffer[256];
   int fresh_count = 0;
-  long *fresh_range = NULL;
+  char **fresh_range = NULL;
   int capacity = 0;
   int range_entries = 0;
-  long start, end;
+  long start, end, id;
 
   printf("AoC day5 #part 1\n");
 
-  // pfile = fopen("input.txt", "r");
-  pfile = fopen("smallInput.txt", "r");
+  pfile = fopen("input.txt", "r");
+  // pfile = fopen("smallInput.txt", "r");
 
   if (!pfile) {
     printf("Error opening file\n");
@@ -26,31 +27,31 @@ int main() {
     if (buffer[0] == '\n') {
       break;
     }
-    sscanf(buffer, "%ld-%ld", &start, &end);
-    // printf("Range: %ld - %ld\n", start, end);
 
-    for (int i = start; i <= end; i++) {
-
-      // allocate more space if needed
-      if (range_entries == capacity) {
-        capacity = (capacity == 0) ? 16 : capacity * 2;
-        fresh_range = realloc(fresh_range, capacity * sizeof(long));
-      }
-
-      fresh_range[range_entries] = i;
-      // printf("Fresh ID: %ld\n", fresh_range[i]);
-      range_entries++;
+    //   // allocate more space if needed
+    if (range_entries == capacity) {
+      capacity = (capacity == 0) ? 16 : capacity * 2;
+      fresh_range = realloc(fresh_range, capacity * sizeof(long));
     }
+
+    buffer[strcspn(buffer, "\n")] = '\0'; // remove newline
+    fresh_range[range_entries] = malloc(strlen(buffer) + 1);
+    strcpy(fresh_range[range_entries], buffer);
+    range_entries++;
+
+    // sscanf(buffer, "%ld-%ld", &start, &end);
+    // printf("Range: %ld - %ld\n", start, end);
   }
 
   start = -1;
   // ingredient ids
   while (fgets(buffer, sizeof(buffer), pfile)) {
-    sscanf(buffer, "%ld", &start);
-    // printf("ID: %ld\n", start);
+    sscanf(buffer, "%ld", &id);
 
     for (int i = 0; i < range_entries; i++) {
-      if (fresh_range[i] == start) {
+      sscanf(fresh_range[i], "%ld-%ld", &start, &end);
+      // printf("Range: %ld - %ld\n", start, end);
+      if (id >= start && id <= end) {
         fresh_count += 1;
         break;
       }
